e6b1156ae4e2faeda98be7a4844673f16bfdd683
Volker Kempert <volker.kempert@almedso.de>
2020-07-09 18:00:59 +0200

    Switch to actix-web

---
+++ src/lib.rs
@@ -0,0 +1,99 @@
+//! Draw a square, circle and triangle on the screen using the `embedded_graphics` crate.
+//!
+//! Run on RaspberryPi Explorer 700 shield with ssd1306 based OLED display
+//!
+
+use embedded_graphics::{
+    geometry::Point,
+    pixelcolor::BinaryColor,
+    prelude::*,
+    primitives::{Circle, Rectangle, Triangle},
+    style::PrimitiveStyleBuilder,
+};
+
+[cfg(cargo_target(unix))]
+use ssd1306::{prelude::*, Builder};
+
+[cfg(cargo_target(unix))]
+use linux_embedded_hal::{
+    spidev::{self, SpidevOptions},
+    sysfs_gpio::Direction,
+    Delay, Pin, Spidev,
+};
+
+[cfg(cargo_target(unix))]
+fn draw_display() -> Result<(), std::io::Error> {
+    // Configure SPI
+    let mut spi = Spidev::open("/dev/spidev0.0").expect("spidev directory");
+    let options = SpidevOptions::new()
+        .bits_per_word(8)
+        .max_speed_hz(8_000_000)
+        .mode(spidev::SpiModeFlags::SPI_MODE_0)
+        .build();
+    spi.configure(&options).expect("spi configuration");
+
+    // Configure Data/Command pin
+    let dc = Pin::new(16);
+    dc.export().expect("dc export");
+    while !dc.is_exported() {}
+    dc.set_direction(Direction::Out).expect("dc Direction");
+    dc.set_value(1).expect("dc Value set to 1");
+
+    // Configure Reset pin
+    let mut rst = Pin::new(19); // pin 18 GPIO 24
+    rst.export().expect("dc export");
+    while !rst.is_exported() {}
+    rst.set_direction(Direction::Out).expect("rst Direction");
+    rst.set_value(1).expect("rst Value set to 1");
+
+    let mut delay = Delay {};
+
+    let mut disp: GraphicsMode<_> = Builder::new()
+        .with_rotation(DisplayRotation::Rotate180)
+        .connect_spi(spi, dc).into();
+
+
+    disp.reset(&mut rst, &mut delay).unwrap();
+    disp.init().unwrap();
+
+    let yoffset = 20;
+
+    let style = PrimitiveStyleBuilder::new()
+        .stroke_width(1)
+        .stroke_color(BinaryColor::On)
+        .build();
+
+    // screen outline
+    // default display size is 128x64 if you don't pass a _DisplaySize_
+    // enum to the _Builder_ struct
+    Rectangle::new(Point::new(0, 0), Point::new(127, 63))
+        .into_styled(style)
+        .draw(&mut disp)
+        .unwrap();
+
+    // triangle
+    Triangle::new(
+        Point::new(16, 16 + yoffset),
+        Point::new(16 + 16, 16 + yoffset),
+        Point::new(16 + 8, yoffset),
+    )
+    .into_styled(style)
+    .draw(&mut disp)
+    .unwrap();
+
+    // square
+    Rectangle::new(Point::new(52, yoffset), Point::new(52 + 16, 16 + yoffset))
+        .into_styled(style)
+        .draw(&mut disp)
+        .unwrap();
+
+    // circle
+    Circle::new(Point::new(96, yoffset + 8), 8)
+        .into_styled(style)
+        .draw(&mut disp)
+        .unwrap();
+
+    disp.flush().unwrap();
+
+    Ok (())
+}
