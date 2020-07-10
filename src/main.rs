608836049e5d4fcc314dcfb34b5cb73525b29577
Volker Kempert <volker.kempert@almedso.de>
2020-07-10 18:29:11 +0200

    Add logging via env-logger and middleware

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+// #[macro_use]
+// extern crate log;
 
+use actix_web::{
+    get, web, App, HttpServer,
+    middleware::Logger,
+    Responder,
+};
+use actix_web::{error, Result};
+use failure::Fail;
+use log::debug;
+
+#[derive(Fail, Debug)]
+#[fail(display = "my error")]
+pub struct MyError {
+    name: &'static str,
+}
+
+// Use default implementation for `error_response()` method
+impl error::ResponseError for MyError {}
+
+async fn error() -> Result<&'static str, MyError> {
+    let err = MyError { name: "test error" };
+    debug!("{}", err);
+    Err(err)
+}
 #[get("/{id}/{name}/index.html")]
 async fn index(info: web::Path<(u32, String)>) -> impl Responder {
     format!("Hello {}! id:{}", info.1, info.0)
@@ -7,7 +31,17 @@ async fn index(info: web::Path<(u32, String)>) -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+
+    std::env::set_var("RUST_LOG", "rpi_hwctl=debug,actix_web=info");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .route("/error", web::get().to(error))
+            .service(index)
+        })
         .bind("127.0.0.1:8088")?
         .run()
         .await
