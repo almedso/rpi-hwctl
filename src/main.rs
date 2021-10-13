2693ce66c6ffa9adaff0bd365cbbbb4244c34a45
Volker Kempert <volker.kempert@almedso.de>
2021-10-13 21:26:05 +0200

    chore: Cleanup and make main compile and run again
    main spawns a tokio thread that loops over a periodic ticks
    main starts a actix web server and serves a minimal id page

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,54 @@
-// #[macro_use]
-// extern crate log;
+use actix_web::{get, web, App, HttpServer, Responder};
+use env_logger;
+use log::{debug, info};
+use std::time::Duration;
+use tokio::{self, time};
 
-use actix_web::{error, Result};
-use actix_web::{get, middleware::Logger, web, App, HttpServer, Responder};
-use failure::Fail;
-use log::debug;
-
-#[derive(Fail, Debug)]
-#[fail(display = "my error")]
-pub struct MyError {
-    name: &'static str,
-}
-
-// Use default implementation for `error_response()` method
-impl error::ResponseError for MyError {}
-
-async fn error() -> Result<&'static str, MyError> {
-    let err = MyError { name: "test error" };
-    debug!("{}", err);
-    Err(err)
-}
 #[get("/{id}/{name}/index.html")]
-async fn index(info: web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", info.1, info.0)
+async fn index(path: web::Path<(u32, String)>) -> impl Responder {
+    let (id, name) = path.into_inner();
+    info!("Hello {}! id:{}", name, id);
+    format!("Hello {}! id:{}", name, id)
 }
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+fn main() {
     std::env::set_var("RUST_LOG", "rpi_hwctl=debug,actix_web=info");
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
+    actix_web::rt::System::with_tokio_rt(|| {
+        tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .worker_threads(8)
+            .thread_name("main-tokio")
+            .build()
+            .unwrap()
+    })
+    .block_on(async_main());
+}
+
+async fn async_main() {
+    tokio::spawn(async {
+        let mut interval = time::interval(Duration::from_millis(1000));
+        let mut tick = 0_u32;
+
+        loop {
+            interval.tick().await;
+            tick += 1;
+            debug!("Another tick passed: {}", tick);
+            // do_something().await;
+        }
+    });
+
     HttpServer::new(|| {
         App::new()
-            .wrap(Logger::default())
-            .route("/error", web::get().to(error))
+            .wrap(actix_web::middleware::Logger::default())
             .service(index)
     })
-    .bind("127.0.0.1:8088")?
+    .workers(8)
+    .bind("0.0.0.0:8088")
+    .expect("Couldn't bind to port 8088")
     .run()
     .await
+    .unwrap()
 }
